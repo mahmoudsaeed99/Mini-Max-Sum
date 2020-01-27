95f7a5df63c4738e0e330ea7fb3fa610b1574d9f
mahmoudsaeed99 <36303424+mahmoudsaeed99@users.noreply.github.com>
2020-01-27 20:18:30 +0200

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.Arrays;
+
+
+public class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        Arrays.sort( arr );
+        long max =0 , min =0;
+        for(int i=0 ; i<arr.length ; i++){
+            if(i>=1){
+                max+=arr[i];
+                
+            }
+            if(i<arr.length-1){
+                min+=arr[i];
+            }
+        }
+        System.out.print(min);
+        System.out.print(" ");
+        System.out.print(max);
+       
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
